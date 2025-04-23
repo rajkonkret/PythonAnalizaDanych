6deea322c541b878e8331472d635624fa19ec6d9
radoslawjaniak <rajkokret660@gmail.com>
2025-04-23 14:10:20 +0200

    [1 8] update kolekcje.py zbiroy

--- kolekcje.py
+++ kolekcje.py
@@ -71,3 +71,97 @@ print(list_copy)
 print(id(nnn_lista))  # 4377891328
 print(id(nowa_lista))  # 4377891328
 print(id(list_copy))  # 4381276288
+
+# krotka (tuple) - niemutowalna lista
+# pozwala lepiej zarządzać pamięcią
+krotka = (23, 34, 54, "Radek")
+print(krotka)  # (23, 34, 54, 'Radek')
+print(krotka[:2])  # (23, 34)
+krotka1 = "radek", "Tomek", "Zenek"
+print(type(krotka1))  # <class 'tuple'>
+
+krotka2 = ("Radek",)
+krotka3 = "Radek",
+krotka4 = "Radek"
+print(type(krotka2), type(krotka3), type(krotka4))  # <class 'tuple'> <class 'tuple'> <class 'str'>
+print(type(()))  # <class 'tuple'>
+
+print(len(krotka))
+# a, b, c = krotka # ValueError: too many values to unpack (expected 3)
+a, b, *c = krotka  # * worek na pozostałe dane
+print(a, b, c)  # 23 34 [54, 'Radek']
+a, *b, c = krotka
+print(a, b, c)  # 23 [34, 54] Radek
+# krotka[5] = 4 # TypeError: 'tuple' object does not support item assignment
+
+# słownik
+# odpowiednik jsona
+oceny = {"Tomek": 4,
+         "Radek": 5,
+         "Agata": 5,
+         "Jacek": 4}
+print(oceny)  # {'Tomek': 4, 'Radek': 5, 'Agata': 5, 'Jacek': 4}
+print(type(oceny))  # <class 'dict'>
+# null -> None
+print(oceny["Tomek"])  # 4
+print(oceny.get("Tomek"))  # 4
+print(oceny.get("tomek"))  # None
+print(oceny.values())
+print(oceny.keys())
+print(oceny.items())
+# dict_values([4, 5, 5, 4])
+# dict_keys(['Tomek', 'Radek', 'Agata', 'Jacek'])
+# dict_items([('Tomek', 4), ('Radek', 5), ('Agata', 5), ('Jacek', 4)])
+oceny["Agata"] = 6
+print(oceny)  # {'Tomek': 4, 'Radek': 5, 'Agata': 6, 'Jacek': 4}
+oceny["Tomek"] = lista_pusta
+print(oceny["Tomek"])  # ['andrzej', 'jakub', 'paulina', 'zenek']
+print(oceny["Tomek"][1])  # jakub
+oceny["Jacek"] = krotka3
+print(oceny)
+# {'Tomek': ['andrzej', 'jakub', 'paulina', 'zenek'], 'Radek': 5, 'Agata': 6, 'Jacek': ('Radek',)}
+print(oceny.pop("Jacek"))  # usunie element Jacek
+print(oceny)  # {'Tomek': ['andrzej', 'jakub', 'paulina', 'zenek'], 'Radek': 5, 'Agata': 6}
+print(oceny.popitem())  # ('Agata', 6) - ostatni
+
+dictionary = {}
+dict_pusty = dict()
+print(dictionary)
+print(type(dict_pusty))
+# {}
+# <class 'dict'>
+
+# set() - zbiór
+# przechowuje uniklne wartości
+# nie posiada indeksu
+zbior1 = {45, 55, 66, 77}
+zbior2 = {45, 55, 166, 177}
+zbior1.add(101)
+zbior1.add(102)
+zbior1.add(105)
+zbior1.add(77)
+zbior1.add(55)
+zbior1.add("55")
+print(zbior1)  # {66, 101, 102, 105, '55', 45, 77, 55}
+
+print(zbior1.difference(zbior2))
+print(zbior2.difference(zbior1))
+# {66, 101, 102, 105, 77, '55'}
+# {177, 166}
+
+print(zbior1.intersection(zbior2))  # {45, 55}
+
+pusty_zbior = set()
+print(pusty_zbior)
+print(type(pusty_zbior))
+# set()
+# <class 'set'>
+
+lista_ze_zbioru = list(zbior1)
+print(lista_ze_zbioru)
+print(type(lista_ze_zbioru))
+# [66, 101, 102, 105, 45, 77, 55, '55']
+# <class 'list'>
+lista_do_zbioru = [66, 101, 102, 105, 45, 77, 77, 55, '55', 102]
+zbior_z_listy = set(lista_do_zbioru)
+print(zbior_z_listy)  # {66, 101, 102, 105, 45, 77, 55, '55'}
