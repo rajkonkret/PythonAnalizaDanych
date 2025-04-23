b7facf106ca82ff5a09697b7f1600f2cb3c5506a
radoslawjaniak <rajkokret660@gmail.com>
2025-04-23 11:54:15 +0200

    [1 4] update pierwszy.py

--- pierwszy.py
+++ pierwszy.py
@@ -23,9 +23,39 @@ print(39 + 14)  # 53, operacja matematyczna
 # TypeError: unsupported operand type(s) for +: 'int' and 'str'
 # # silne typowanie
 
-print(type(39)) # <class 'int'>, liczby calkowite
+print(type(39))  # <class 'int'>, liczby calkowite
 print(sys.int_info)
 # sys.int_info(bits_per_digit=30,
 #              sizeof_digit=4,
 #              default_max_str_digits=4300,
 #              str_digits_check_threshold=640)
+
+# zmienna - pudełko na dane
+# snake_case
+# kebab-case
+
+# typowanie dynamiczne
+name = "Radek"
+print(name)  # Radek
+print(type(name))  # <class 'str'>
+
+name = 67
+print(name)
+print(type(name))  # <class 'int'>
+
+age = 48
+print(age)
+print(type(age))  # <class 'int'>
+
+
+# podpowiedzi typów
+name: str = 90
+print(type(name))
+print(name) # 90
+name = "Radek"
+# mypy
+# pip install mypy
+# (.venv) radoslawjaniak@mac PythonAnalizaDanych % mypy pierwszy.py
+# pierwszy.py:42: error: Incompatible types in assignment (expression has type "int", variable has type "str")  [assignment]
+# pierwszy.py:52: error: Name "name" already defined on line 38  [no-redef]
+# Found 2 errors in 1 file (checked 1 source file)
